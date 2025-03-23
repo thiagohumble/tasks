92305216c4605077363560b2aad2f05608f4f492
thiago <thg.humble@gmail.com>
2025-03-23 13:19:04 -0300

    created structure backend api

---
+++ task-api/index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { Tasks } = require('./models');
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// task create
+app.post('/tasks', async (req, res) => {
+  try {
+    const task = await Tasks.create(req.body);
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// task list
+app.get('/tasks', async (req, res) => {
+  try {
+    const task = await Tasks.findAll();
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// task edit
+app.put('/tasks/:id', async (req, res) => {
+  try {
+    await Tasks.update(req.body, { where: { id: req.params.id } });
+    res.json({ message: 'Tarefa atualizada' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// task delete
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    await Tasks.destroy({ where: { id: req.params.id } });
+    res.json({ message: 'Tarefa excluída' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// task done
+app.patch('/tasks/:id/concluida', async (req, res) => {
+  try {
+    await Tasks.update({ concluida: true }, { where: { id: req.params.id } });
+    res.json({ message: 'Tarefa marcada como concluída' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
