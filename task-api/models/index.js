d40293cc06a5c9b433057c34d4d2a83bd07903f0
thiago <thg.humble@gmail.com>
2025-03-25 18:57:12 -0300

    changes using just environment variavel

--- task-api/models/index.js
+++ task-api/models/index.js
@@ -5,27 +5,21 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env]; 
 const db = {};
-
 
 let sequelize;
 if (process.env.DATABASE_URL) {
-  // ...
-} else {
-  try {
-    console.log("__dirname:", __dirname); 
-    const config = require(__dirname + '/../config/config.json')[env];
-    console.log("Config (Local):", config); 
-    if (config && config.database) {
-      sequelize = new Sequelize(config.database, config.username, config.password, config);
-    } else {
-      console.error("Error: config.json or database property not found in local configuration.");
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
-  } catch (error) {
-    console.error("Error loading config.json:", error);
-  }
+  });
+} else {
+  console.error("Error: DATABASE_URL environment variable is not defined.");
 }
 
 fs
@@ -53,4 +47,3 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
-
