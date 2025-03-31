2dd815a154c6770f65fec8cb80908f144350798b
thiago <thg.humble@gmail.com>
2025-03-31 08:40:53 -0300

    reverse

--- task-api/models/task.js
+++ task-api/models/task.js
@@ -9,36 +9,9 @@ module.exports = (sequelize, DataTypes) => {
   }
   
   Task.init({
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: {
-          msg: 'O título é obrigatório'
-        },
-        len: {
-          args: [1, 255],
-          msg: 'O título deve ter entre 1 e 255 caracteres'
-        }
-      }
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      validate: {
-        notEmpty: {
-          msg: 'A descrição é obrigatória'
-        }
-      }
-    },
-    done: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    }
+    title: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    done: DataTypes.BOOLEAN
   }, {
     sequelize,
     modelName: 'Task',
