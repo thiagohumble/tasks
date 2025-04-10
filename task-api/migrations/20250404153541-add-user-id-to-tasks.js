50ee700bee505d061a8c30fcb9d014050f72607b
Thiago Humble <thg.humble@gmail.com>
2025-04-10 11:16:13 -0300

    Update 20250404153541-add-user-id-to-tasks.js

--- task-api/migrations/20250404153541-add-user-id-to-tasks.js
+++ task-api/migrations/20250404153541-add-user-id-to-tasks.js
@@ -4,7 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('tasks', 'userId', {
       type: Sequelize.INTEGER,
-      allowNull: true,
+      allowNull: false,
       references: {
         model: 'Users',
         key: 'id',
