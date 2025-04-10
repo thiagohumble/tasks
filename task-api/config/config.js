07593f9f5ae39e6c7af0b6bc692bdbaab9801b0e
thiago <thg.humble@gmail.com>
2025-04-10 13:39:35 -0300

    change of data base

--- task-api/config/config.js
+++ task-api/config/config.js
@@ -1,36 +1,33 @@
 require('dotenv').config();
 
-console.log("config.js is being executed!");
-
 const isProduction = process.env.NODE_ENV === 'production';
 
 let config;
 
 if (isProduction) {
-  // Configuração de produção (usando DATABASE_URL)
+  // Configuração para HostGator
   config = {
-    dialect: 'postgres',
-    use_env_variable: 'DATABASE_URL',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
+    username: process.env.DB_USER || 'humble80_usuario', 
+    password: process.env.DB_PASSWORD || 'W)ss)z1}7vH3', 
+    database: process.env.DB_NAME || 'humble80_tasks',
+    host: process.env.DB_HOST || 'br1020.hostgator.com.br', 
+    dialect: 'mysql', 
+    port: process.env.DB_PORT || 3306 
   };
 } else {
-  // Configuração local (usando config.json)
+  // Configuração local (desenvolvimento)
   config = {
-    username: process.env.DB_USER || 'postgres', // Use variáveis de ambiente ou defaults
+    username: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_NAME || 'tasks',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'postgres',
-    port: process.env.DB_PORT || 5432, // Adicione a porta 5432
+    port: process.env.DB_PORT || 5432,
   };
 }
 
 module.exports = {
   development: config,
   production: config,
+  test: config
 };
